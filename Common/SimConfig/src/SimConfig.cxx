a5c4baca10ae20ea4499dc9b00eba050d065946b
Sandro Wenzel <sandro.wenzel@cern.ch>
2018-06-22 20:41:53 +0200

    Possibility to disable modules

--- Common/SimConfig/src/SimConfig.cxx
+++ Common/SimConfig/src/SimConfig.cxx
@@ -18,35 +18,49 @@ namespace bpo = boost::program_options;
 
 void SimConfig::initOptions(boost::program_options::options_description& options)
 {
-  options.add_options()(
-    "mcEngine,e", bpo::value<std::string>()->default_value("TGeant3"), "VMC backend to be used.")(
-    "generator,g", bpo::value<std::string>()->default_value("boxgen"), "Event generator to be used.")(
-    "modules,m", bpo::value<std::vector<std::string>>()->multitoken()->default_value(
-                   std::vector<std::string>({ "all" }), "all modules"),
-    "list of detectors")("nEvents,n", bpo::value<unsigned int>()->default_value(1), "number of events")(
-    "startEvent", bpo::value<unsigned int>()->default_value(0), "index of first event to be used (when applicable)")(
-    "extKinFile", bpo::value<std::string>()->default_value("Kinematics.root"),
-    "name of kinematics file for event generator from file (when applicable)")(
-    "bMax,b", bpo::value<float>()->default_value(0.), "maximum value for impact parameter sampling (when applicable)")(
-    "isMT", bpo::value<bool>()->default_value(false), "multi-threaded mode (Geant4 only")(
-    "outPrefix,o", bpo::value<std::string>()->default_value("o2sim"), "prefix of output files")(
-	"chunkSize", bpo::value<unsigned int>()->default_value(10000),
-	  "max size of primary chunk (subevent) distributed by server")(
-	"chunkSizeI", bpo::value<int>()->default_value(-1),
-	  "internalChunkSize");
+  using std::string;
+  using std::vector;
+  options.add_options()
+   ("mcEngine,e", bpo::value<string>()->default_value("TGeant3"), "VMC backend to be used.")
+   ("generator,g", bpo::value<string>()->default_value("boxgen"), "Event generator to be used.")
+   ("modules,m", bpo::value<vector<string>>()->multitoken()->default_value(vector<string>({ "all" }), "all modules"),
+    "list of detectors")
+   ("disable,d", bpo::value<vector<string>>()->multitoken()->default_value(vector<string>({ "none" }), "no module"),
+     "list of disabled modules (takes precedence)")
+   ("nEvents,n", bpo::value<unsigned int>()->default_value(1), "number of events")
+   ("startEvent", bpo::value<unsigned int>()->default_value(0), "index of first event to be used (when applicable)")
+   ("extKinFile", bpo::value<string>()->default_value("Kinematics.root"),
+    "name of kinematics file for event generator from file (when applicable)")
+	("bMax,b", bpo::value<float>()->default_value(0.), "maximum value for impact parameter sampling (when applicable)")
+	("isMT", bpo::value<bool>()->default_value(false), "multi-threaded mode (Geant4 only")
+	("outPrefix,o", bpo::value<string>()->default_value("o2sim"), "prefix of output files")
+	("chunkSize", bpo::value<unsigned int>()->default_value(10000),"max size of primary chunk (subevent) distributed by server")
+	("chunkSizeI", bpo::value<int>()->default_value(-1),"internalChunkSize");
 }
 
 bool SimConfig::resetFromParsedMap(boost::program_options::variables_map const& vm)
 {
+  using std::string;
+  using std::vector;
   using o2::detectors::DetID;
-  mConfigData.mMCEngine = vm["mcEngine"].as<std::string>();
-  mConfigData.mActiveDetectors = vm["modules"].as<std::vector<std::string>>();
+  mConfigData.mMCEngine = vm["mcEngine"].as<string>();
+  mConfigData.mActiveDetectors = vm["modules"].as<vector<string>>();
+  auto& disabled = vm["disable"].as<vector<string>>();
   if (mConfigData.mActiveDetectors.size() == 1 && mConfigData.mActiveDetectors[0] == "all") {
     mConfigData.mActiveDetectors.clear();
     for (int d = DetID::First; d <= DetID::Last; ++d) {
       mConfigData.mActiveDetectors.push_back(DetID::getName(d));
     }
   }
+  // take out deactivated modules
+  for (auto& m : disabled) {
+    auto& active = mConfigData.mActiveDetectors;
+    auto iter = std::find(active.begin(), active.end(), m);
+    if (iter != active.end()) {
+      active.erase(iter);
+    }
+  }
+
   mConfigData.mGenerator = vm["generator"].as<std::string>();
   mConfigData.mNEvents = vm["nEvents"].as<unsigned int>();
   mConfigData.mExtKinFileName = vm["extKinFile"].as<std::string>();
